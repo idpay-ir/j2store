e6780415f4a2e22cf7645330ab8af0c42f4a7b61
Imi Kiani <kianiman@gmail.com>
2019-02-19 17:48:23 +0330

    coding standards

--- payment_idpay/payment_idpay/tmpl/postpayment.php
+++ payment_idpay/payment_idpay/tmpl/postpayment.php
@@ -2,15 +2,15 @@
 /**
  * IDPay payment plugin
  *
- * @developer JMDMahdi
- * @publisher IDPay
- * @package VirtueMart
- * @subpackage payment
+ * @developer     JMDMahdi
+ * @publisher     IDPay
+ * @package       VirtueMart
+ * @subpackage    payment
  * @copyright (C) 2018 IDPay
- * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ * @license       http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
  *
  * http://idpay.ir
  */
- defined('_JEXEC') or die('Restricted access'); ?>
+defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 
-<div class="note"><?php echo JText::_($vars->onafterpayment_text); ?></div>
+<div class="note"><?php echo JText::_( $vars->onafterpayment_text ); ?></div>
