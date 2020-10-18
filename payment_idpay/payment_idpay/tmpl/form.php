d77bf89b7db1c8aa4eb92dfdbc482bc8c67bb108
Meysam Razmi <m.razmi.92@gmail.com>
2020-10-18 15:38:25 +0330

    do some code modifications and bug fixes and translation improvment and handle GET and POST methods.

---
+++ payment_idpay/payment_idpay/tmpl/form.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * IDPay payment plugin
+ *
+ * @developer     JMDMahdi, meysamrazmi, vispa
+ * @publisher     IDPay
+ * @package       J2Store
+ * @subpackage    payment
+ * @copyright (C) 2020 IDPay
+ * @license       http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * https://idpay.ir
+ */
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<img src="/plugins/j2store/payment_idpay/payment_idpay/logo.svg" style="display: inline-block;vertical-align: middle;width: 50px;">
+<?php echo JText::_("PLG_J2STORE_IDPAY_OPTION_NAME"); ?>
+
---
+++ payment_idpay/payment_idpay/tmpl/postpayment.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * IDPay payment plugin
+ *
+ * @developer     JMDMahdi, meysamrazmi, vispa
+ * @publisher     IDPay
+ * @package       J2Store
+ * @subpackage    payment
+ * @copyright (C) 2020 IDPay
+ * @license       http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * https://idpay.ir
+ */
+
+ defined('_JEXEC') or die('Restricted access'); ?>
+
+<div class="note idpay-message"><?php echo JText::_($vars->message); ?></div>
